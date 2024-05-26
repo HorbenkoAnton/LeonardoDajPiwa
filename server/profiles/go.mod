a09fb05b973826871e3cd4cb2e2eaeb87caf46a1
sizuyax <gongband06@gmail.com>
2024-05-26 18:04:01 +0200

    added custom handler for logger

--- server/profiles/go.mod
+++ server/profiles/go.mod
@@ -5,6 +5,7 @@ go 1.22.2
 require (
 	github.com/jackc/pgx/v5 v5.5.5
 	github.com/pressly/goose v2.7.0+incompatible
+	golang.org/x/net v0.21.0
 	google.golang.org/grpc v1.63.2
 	google.golang.org/protobuf v1.33.0
 )
@@ -15,7 +16,6 @@ require (
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- server/profiles/lib/logger/logger.go
+++ server/profiles/lib/logger/logger.go
@@ -1,10 +1,85 @@
 package logger
 
 import (
+	"fmt"
+	"golang.org/x/net/context"
+	"io"
 	"log/slog"
 	"os"
 	"strings"
+	"sync"
 )
+
+type HandlerOptions struct {
+	TimestampFormat string
+	Level           slog.Level
+}
+
+type CustomHandler struct {
+	w    io.Writer
+	opts HandlerOptions
+	mu   *sync.Mutex
+}
+
+const (
+	TimeFormat = "2006-01-02 15:04:05"
+)
+
+func NewCustomHandler(w io.Writer, opts *HandlerOptions) *CustomHandler {
+	if opts == nil {
+		opts = &HandlerOptions{
+			TimestampFormat: TimeFormat,
+			Level:           slog.LevelInfo,
+		}
+	}
+	return &CustomHandler{
+		w:    w,
+		opts: *opts,
+		mu:   &sync.Mutex{},
+	}
+}
+
+func (h *CustomHandler) Handle(ctx context.Context, r slog.Record) error {
+	if !h.Enabled(ctx, r.Level) {
+		return nil
+	}
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	ts := r.Time.Format(h.opts.TimestampFormat)
+	level := r.Level.String()
+	msg := r.Message
+	attrs := formatAttrs(r)
+
+	if attrs == "" {
+		fmt.Printf("%s | %s | %s\n", ts, level, msg)
+		return nil
+	}
+
+	fmt.Printf("%s | %s | %s | %s\n", ts, level, msg, attrs)
+	return nil
+}
+
+func formatAttrs(r slog.Record) string {
+	var sb strings.Builder
+	r.Attrs(func(attr slog.Attr) bool {
+		sb.WriteString(fmt.Sprintf("%s=%v ", attr.Key, attr.Value))
+		return true
+	})
+	return sb.String()
+}
+
+func (h *CustomHandler) Enabled(_ context.Context, level slog.Level) bool {
+	return level >= h.opts.Level
+}
+
+func (h *CustomHandler) WithAttrs(_ []slog.Attr) slog.Handler {
+	return h
+}
+
+func (h *CustomHandler) WithGroup(_ string) slog.Handler {
+	return h
+}
 
 func SetupLogger(logLevel string) *slog.Logger {
 	var log *slog.Logger
@@ -12,13 +87,17 @@ func SetupLogger(logLevel string) *slog.Logger {
 
 	if logLevel == "dev" {
 		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+			NewCustomHandler(os.Stdout, &HandlerOptions{TimestampFormat: TimeFormat, Level: slog.LevelDebug}),
 		)
 		return log
 	}
 
 	log = slog.New(
+<<<<<<< HEAD
 		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+=======
+		NewCustomHandler(os.Stdout, &HandlerOptions{TimestampFormat: TimeFormat, Level: slog.LevelWarn}),
+>>>>>>> 4ba3fc9 (added custom handler for logger)
 	)
 
 	return log
